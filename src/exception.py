332c6cf3cc7a324e67c620436f2954e460a540c2
Kalyan Pichumani <kalyanpichumani@192.168.1.6>
2025-05-02 14:29:12 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,19 @@
+import sys # sys library will have al the info about exception
+import logging
+def error_message_detail(error,error_detail:sys):
+    _,_,exe_tb = error_detail.exc_info()
+    file_name = exe_tb.tb_frame.f_code.co_filename
+    # this gives details on which file, line no has the exception
+    error_msg = "Error occured in python script name[{0}] line number  [{1}] error message[{2}]".format(file_name,exe_tb.tb_lineno,str(error))
+    return error_msg
+
+class CustomException(Exception):
+
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __str__(self):
+        return self.error_message
+    
+
+    
