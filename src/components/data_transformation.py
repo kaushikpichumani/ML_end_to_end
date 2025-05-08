e9f1bd19e376ca9aa3e5ea3784163928e10bb2d5
Kalyan Pichumani <kalyanpichumani@Kalyans-MacBook-Pro.local>
2025-05-08 08:24:03 +0530

    update to model file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -80,6 +80,7 @@ class DataTransformation:
             test_arr = np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
 
             logging.info("saved preprrocessing object")
+            print("saved preprrocessing object")
 
             save_object(file_path=self.data_transformation_config.preprocessor_obj_file_path,
                         obj = preprocessor_obj)
